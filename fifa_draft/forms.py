15c948fee803004fc81c028518ad29ab9767a016
grzegorz-oledzki <you@example.com>
2022-05-04 19:48:42 +0200

    feat: add working form, add error messeges, change in model, add Groups to admin panel

--- fifa_draft/forms.py
+++ fifa_draft/forms.py
@@ -6,3 +6,9 @@ class GroupForm(ModelForm):
         model = Group
         owner = Profile
         fields = ['name', 'description', 'number_of_players', 'password', 'featured_image', 'owner']
+
+    def __init__(self, *args, **kwargs):
+        super(GroupForm, self).__init__(*args, **kwargs)
+
+        for name, field in self.fields.items():
+            field.widget.attrs.update({"class": "input", "placeholder": "Add " + name})
--- fifa_draft/views.py
+++ fifa_draft/views.py
@@ -1,12 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from fifa_draft.forms import GroupForm
+from fifa_draft.models import Profile, Group
+from django.contrib import messages
+
 
 def home(request):
     return render(request, 'home.html')
 
 def create_group(request):
     form = GroupForm()
-    # if request.method == "POST":
-    #     form = GroupForm(request.POST)
+    if request.method == "POST":
+        form = GroupForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            messages.error(request, 'Only number of player from 14 to 20 are accepted.')
     context = {'form': form}
     return render(request, 'group-form.html', context)
