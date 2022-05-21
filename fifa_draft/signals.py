8de31e5968d4c3606b2a3d7f8a825bad421d4893
grzegorz-oledzki <you@example.com>
2022-05-21 14:48:50 +0200

    feat: sending mail to new users function

--- fifa_draft/signals.py
+++ fifa_draft/signals.py
@@ -1,6 +1,8 @@
 from django.db.models.signals import post_save, post_delete
 from fifa_draft.models import Profile
 from django.contrib.auth.models import User
+from django.core.mail import send_mail
+from django.conf import settings
 
 
 def create_profile(sender, instance, created, **kwargs):
@@ -8,6 +10,15 @@ def create_profile(sender, instance, created, **kwargs):
         user = instance
         profile = Profile.objects.create(
             user=user, username=user.username, email=user.email, name=user.first_name
+        )
+        subject = "Welcome to Fifa draft page!"
+        message = "We are glad you are here!"
+        send_mail(
+            subject,
+            message,
+            settings.EMAIL_HOST_USER,
+            [profile.email],
+            fail_silently=False,
         )
 
 
