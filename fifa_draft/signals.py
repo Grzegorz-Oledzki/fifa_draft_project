2b326eec8d0629eadf2d0e4a36c85b803717d5f5
grzegorz-oledzki <you@example.com>
2022-05-29 14:20:02 +0200

    feat: add forget password pages, fix table search

--- fifa_draft/signals.py
+++ fifa_draft/signals.py
@@ -3,6 +3,8 @@ from fifa_draft.models import Profile
 from django.contrib.auth.models import User
 from django.core.mail import send_mail
 from django.conf import settings
+from django.contrib import messages
+
 
 
 def create_profile(sender, instance, created, **kwargs):
@@ -27,5 +29,16 @@ def delete_profile(sender, instance, **kwargs):
     user.delete()
 
 
+def update_user(sender, instance, created, **kwargs):
+    profile = instance
+    user = profile.user
+    if created == False:
+        user.first_name = profile.name
+        user.username = profile.username
+        user.email = profile.email
+        user.save()
+
+
 post_save.connect(create_profile, sender=User)
 post_delete.connect(delete_profile, sender=Profile)
+post_save.connect(update_user, sender=Profile)
