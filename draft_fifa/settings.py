4ca5b28b08687ddeefbe1e80ca8d6793b210b062
grzegorz-oledzki <you@example.com>
2022-05-30 17:44:19 +0200

    before

--- draft_fifa/settings.py
+++ draft_fifa/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = "django-insecure-o%su%rl!n)-8tmq$0noowg!j*_o&6*-)2h-6_q=(*j%_fyixuk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', 'fifa-draft.herokuapp.com', ".herokuapp.com"]
+ALLOWED_HOSTS = ['localhost', 'fifa-draft.herokuapp.com', ".herokuapp.com", '127.0.0.1']
 
 
 # Application definition
@@ -79,22 +79,22 @@ WSGI_APPLICATION = "draft_fifa.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "grzesdb1",
-        "USER": 'grzes',
-        "PASSWORD": config("PASSWORD"),
-        "HOST": "grzesdb1.cjrjzpvmnlda.eu-west-1.rds.amazonaws.com",
-        "PORT": "5432",
-    }
-}
 # DATABASES = {
 #     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": "postgres",
+#         "USER": 'grzes',
+#         "PASSWORD": config("PASSWORD"),
+#         "HOST": "grzesdb1.cjrjzpvmnlda.eu-west-1.rds.amazonaws.com",
+#         "PORT": "5432",
 #     }
 # }
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db1.sqlite3",
+    }
+}
 
 
 
