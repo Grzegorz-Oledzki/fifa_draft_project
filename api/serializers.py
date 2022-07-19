1cbbcf1ecd1a5ecfea2791155612f2d021a8b708
grzegorz-oledzki <you@example.com>
2022-07-19 20:43:11 +0200

    feat: setup django rest framework for all models

--- api/serializers.py
+++ api/serializers.py
@@ -8,3 +8,21 @@ class TeamSerializer(serializers.ModelSerializer):
     class Meta:
         model = Team
         fields = '__all__'
+
+
+class GroupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Group
+        fields = '__all__'
+
+
+class PlayerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Player
+        fields = '__all__'
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = '__all__'
--- api/urls.py
+++ api/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     path('', views.get_routes),
     path('teams/', views.get_teams),
     path('team/<str:pk>/', views.get_team),
+    path('groups/', views.get_groups),
+    path('group/<str:pk>/', views.get_group),
+    path('player/<str:pk>/', views.get_player),
+    path('user/<str:pk>/', views.get_profile),
 ]
