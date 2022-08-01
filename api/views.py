73684f8ea1e7afea1d8199712a7b64b841fdc68b
grzegorz-oledzki <you@example.com>
2022-08-01 22:24:35 +0200

    black formatting

--- api/views.py
+++ api/views.py
@@ -1,25 +1,29 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from api.serializers import TeamSerializer, GroupSerializer, PlayerSerializer, ProfileSerializer
+from api.serializers import (
+    TeamSerializer,
+    GroupSerializer,
+    PlayerSerializer,
+    ProfileSerializer,
+)
 from fifa_draft.models import Team, Group
 from players.models import Player
 from users.models import Profile
 
 
-@api_view(['GET', 'POST'])
+@api_view(["GET", "POST"])
 def get_routes(request):
 
     routes = [
-        {'GET': '/api/groups'},
-        {'GET': '/api/group/id'},
-        {'GET': '/api/team/id'},
-        {'GET': '/api/players/choose-team'},
-        {'GET': '/api/players/players-pick/id'},
-        {'POST': '/api/players/pending-player-pick-confirmation/sofifa_id/id'},
-        {'POST': '/api/players/player-pick-confirmation/sofifa_id/id'},
-        {'POST': '/api/users/token'},
-        {'POST': '/api/users/token/refresh'},
-
+        {"GET": "/api/groups"},
+        {"GET": "/api/group/id"},
+        {"GET": "/api/team/id"},
+        {"GET": "/api/players/choose-team"},
+        {"GET": "/api/players/players-pick/id"},
+        {"POST": "/api/players/pending-player-pick-confirmation/sofifa_id/id"},
+        {"POST": "/api/players/player-pick-confirmation/sofifa_id/id"},
+        {"POST": "/api/users/token"},
+        {"POST": "/api/users/token/refresh"},
     ]
 
     return Response(routes)
@@ -32,7 +36,7 @@ def get_teams(request):
     return Response(serializer.data)
 
 
-@api_view(['GET'])
+@api_view(["GET"])
 def get_team(request, pk):
     team = Team.objects.get(id=pk)
     serializer = TeamSerializer(team, many=False)
