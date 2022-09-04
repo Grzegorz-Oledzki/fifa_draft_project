12f185baae8ba21fe8f9e037a4605728f589a3ef
grzegorz-oledzki <you@example.com>
2022-09-04 13:07:10 +0200

    refactor: correct typing, used black

--- api/utils.py
+++ api/utils.py
@@ -8,9 +8,8 @@ from users.models import Profile
 
 def team_form_validation_api(team_password, profile: Profile, belongs_group) -> bool:
     unique_name = is_unique_name
-    if (
+    return (
         belongs_group.password == team_password
         and profile not in belongs_group.members.all()
         and unique_name
-    ):
-        return True
+    )
