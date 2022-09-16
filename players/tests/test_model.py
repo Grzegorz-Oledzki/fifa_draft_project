a3ed1c3b67688e862aaebdae23c7650ad77754e6
grzegorz-oledzki <you@example.com>
2022-09-16 17:04:05 +0200

    feat: test player model

---
+++ players/tests/test_model.py
@@ -0,0 +1,45 @@
+from django.test import TestCase
+import os
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'draft_fifa.settings')
+django.setup()
+
+from players.models import Player
+
+data = {
+    "id": 127,
+    "sofifa_id": 111111,
+    "player_url": "",
+    "short_name": "G. Olędzki",
+    "player_positions": "CF, ST",
+    "overall": 69,
+    "age": 27,
+    "height_cm": 184,
+    "weight_kg": 96,
+    "club_name": "FC Katowice",
+    "preferred_foot": "Left",
+    "weak_foot": 2,
+    "skill_moves": 2,
+    "work_rate": "Medium/Medium",
+    "pace": 70,
+    "shooting": 72,
+    "passing": 67,
+    "dribbling": 68,
+    "defending": 64,
+    "physic": 80,
+    "player_face_url": "",
+    "club_logo_url": "",
+    "nation_flag_url": "https://cdn.sofifa.net/flags/pl.png"
+}
+
+
+class TestPlayer(TestCase):
+
+    def test_get_player_by_na(self) -> None:
+        player = Player.objects.create(**data)
+        self.assertEqual(player.short_name, data['short_name'])
+
+    def test_get_player_by_sofifa_id(self) -> None:
+        player = Player.objects.create(**data)
+        self.assertEqual(player.sofifa_id, data['sofifa_id'])
