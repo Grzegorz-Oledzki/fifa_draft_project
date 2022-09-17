60791b36490fb0fd84e481aed8dc14659211de5e
grzegorz-oledzki <you@example.com>
2022-09-17 13:42:36 +0200

    refactor: change test for pytest method

--- fifa_draft/tests/test_models.py
+++ fifa_draft/tests/test_models.py
@@ -1,24 +1,18 @@
 import os
+from typing import Tuple
 
 import django
-from django.test import TestCase
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "draft_fifa.settings")
 django.setup()
-
 from fifa_draft.models import Group, Team
+from django.contrib.auth.models import User
 from users.models import Profile
 
 profile_data = {
-    "name": "Grzesiu Test",
-    "username": "grzes test",
+    "username": "grzes te11",
+    "password": "abc"
 }
-
-profile_data2 = {
-    "name": "Grzesiu Test 2",
-    "username": "grzes test 2",
-}
-
 group_data = {
     "name": "Test group",
     "number_of_players": 15,
@@ -28,42 +22,40 @@ team_data = {
     "name": "Test team",
     "formation": "4–3–2–1",
 }
-team_data2 = {
-    "name": "Test team",
-    "formation": "4–3–3",
-}
 
 
-class TestPlayerUtils(TestCase):
-    def setUp(self) -> None:
-        self._create_profiles_group_and_team_and_connect_them()
+def _create_user_and_profile() -> Profile:
+    user = User.objects.create(**profile_data)
+    profile = Profile.objects.get(id=user.profile.id)
+    return profile
 
-    def test_get_profile(self) -> None:
-        self.assertEqual(type(self.profile), Profile)
 
-    def test_get_group_name_and_owner(self) -> None:
-        self.assertEqual(self.group.name, group_data["name"])
-        self.assertEqual(self.group.owner, self.profile)
+def _create_team_group_and_profile() -> Tuple[Team, Group, Profile]:
+    profile = _create_user_and_profile()
+    group = Group.objects.create(**group_data, owner=profile)
+    team = Team.objects.create(**team_data, owner=profile, belongs_group=group)
+    return team, group, profile
 
-    def test_get_team_name_and_owner(self) -> None:
-        self.assertEqual(self.team.name, team_data["name"])
-        self.assertEqual(self.team.owner, self.profile)
 
-    def test_if_team_is_in_group(self) -> None:
-        self.assertEqual(self.team.belongs_group, self.group)
+def _delete_profile_and_group(profile: Profile, group: Group) -> None:
+    profile.delete()
+    group.delete()
 
-    def test_add_second_team_to_group(self) -> None:
-        self.team2 = Team.objects.create(
-            **team_data2, owner=self.profile2, belongs_group=self.group
-        )
-        self.assertEqual(self.team2.belongs_group, self.group)
-        self.assertEqual(self.team2.owner, self.profile2)
 
-    def _create_profiles_group_and_team_and_connect_them(self):
-        self.profile = Profile.objects.create(**profile_data)
-        self.profile2 = Profile.objects.create(**profile_data2)
-        self.group = Group.objects.create(**group_data, owner=self.profile)
-        self.group.members.add(self.profile)
-        self.team = Team.objects.create(
-            **team_data, owner=self.profile, belongs_group=self.group
-        )
+def test_create_group(tmp_path) -> None:
+    profile = _create_user_and_profile()
+    group = Group.objects.create(**group_data, owner=profile)
+    assert type(group) == Group
+    _delete_profile_and_group(profile, group)
+
+
+def test_create_team() -> None:
+    team, group, profile = _create_team_group_and_profile()
+    assert type(team) == Team
+    _delete_profile_and_group(profile, group)
+
+
+def test_add_team_to_group() -> None:
+    team, group, profile = _create_team_group_and_profile()
+    assert team.belongs_group == group
+    _delete_profile_and_group(profile, group)
